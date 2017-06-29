0eb32a1e31b4597591a3609ab9316239f08e14dc
Justus Adam <dev@justus.science>
2017-06-29 11:13:00 +0200

    Changed decreaseIndentPattern to skip keywords with `'`

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
 		
         indentationRules: {
             // ^.*\{[^}"']*$
-            increaseIndentPattern: /((\b(if\b.*|then|else|m?do|of|let|in|where))|=|->|>>=|>=>|=<<|(^(data)( |\t)+(\w|')+( |\t)*))( |\t)*$/,
+            increaseIndentPattern: /(\bif\b(?!').*|\b(then|else|m?do|of|let|in|where)\b(?!')|=|->|>>=|>=>|=<<|(^(data)( |\t)+(\w|')+( |\t)*))( |\t)*$/,
             decreaseIndentPattern: null
         },
         wordPattern: /([\w'_][\w'_\d]*)|([0-9]+\.[0-9]+([eE][+-]?[0-9]+)?|[0-9]+[eE][+-]?[0-9]+)/
