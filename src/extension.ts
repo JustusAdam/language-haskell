c9bc9b61803cd8476dbf7c03f7c58c9b4e8eb40e
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2017-09-28 09:49:58 -0700

    Add haskell.indentationRules.enabled setting.

--- src/extension.ts
+++ src/extension.ts
@@ -3,12 +3,12 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-
     vscode.languages.setLanguageConfiguration('haskell', {
-		
         indentationRules: {
             // ^.*\{[^}"']*$
-            increaseIndentPattern: /(\bif\b(?!')(.(?!then))*|\b(then|else|m?do|of|let|in|where)\b(?!')|=|->|>>=|>=>|=<<|(^(data)( |\t)+(\w|')+( |\t)*))( |\t)*$/,
+            increaseIndentPattern: vscode.workspace.getConfiguration('haskell').indentationRules.enabled
+                ? /(\bif\b(?!')(.(?!then))*|\b(then|else|m?do|of|let|in|where)\b(?!')|=|->|>>=|>=>|=<<|(^(data)( |\t)+(\w|')+( |\t)*))( |\t)*$/
+                : null,
             decreaseIndentPattern: null
         },
         wordPattern: /([\w'_][\w'_\d]*)|([0-9]+\.[0-9]+([eE][+-]?[0-9]+)?|[0-9]+[eE][+-]?[0-9]+)/
