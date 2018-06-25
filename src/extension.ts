c01bacc62b56f659caba58cbf57d265cd26469bf
Tsvetan Ovedenski <dev@tsovedenski.com>
2018-06-25 22:33:02 +0200

    Remove indentation after 2 empty newlines

--- src/extension.ts
+++ src/extension.ts
@@ -12,5 +12,40 @@ export function activate(context: vscode.ExtensionContext) {
             decreaseIndentPattern: null
         },
         wordPattern: /([\w'_][\w'_\d]*)|([0-9]+\.[0-9]+([eE][+-]?[0-9]+)?|[0-9]+[eE][+-]?[0-9]+)/
-    })  	
+    });
+
+    vscode.workspace.onDidChangeTextDocument(removeIndentAfter2Lines);
 }
+
+const removeIndentAfter2Lines = (event: vscode.TextDocumentChangeEvent) => {
+    const document = event.document;
+
+    if (document.languageId !== 'haskell') {
+        return;
+    }
+
+    if (event.contentChanges.length < 1) {
+        return;
+    }
+
+    const line = event.contentChanges[0].range.start.line;
+    if (line < 1) {
+        return;
+    }
+
+    const prevLine = document.lineAt(line);
+    const currLine = document.lineAt(line + 1);
+
+    const shouldRemoveIndent = indentedEmpty(prevLine, -1) 
+                            && indentedEmpty(currLine, 0);
+
+    if (shouldRemoveIndent) {
+        const edit = new vscode.WorkspaceEdit();
+        edit.replace(document.uri, currLine.range, '');
+        vscode.workspace.applyEdit(edit);
+    }
+}
+
+const indentedEmpty = (line: vscode.TextLine, threshold: number) => 
+               line.isEmptyOrWhitespace 
+            && line.firstNonWhitespaceCharacterIndex > threshold;
