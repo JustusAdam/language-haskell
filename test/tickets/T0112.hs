6defacef69b65639765545d5399f99b836f46521
sheaf <sam.derbyshire@gmail.com>
2020-04-30 20:30:10 +0200

    allow pragmas in module declaration headers

--- test/tickets/T0112.hs
+++ test/tickets/T0112.hs
@@ -2,7 +2,7 @@
 
 module Web.Telegram.API.Lens {-# dEPrecatED "use labels from generic-lens instead" #-}
 --                               ^^^^^^^^^^ meta.preprocessor.haskell keyword.other.preprocessor.haskell
---                                           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.preprocessor.haskell string.quoted.double.haskell
+--                                          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.preprocessor.haskell string.quoted.double.haskell
   where
 
 {-# dEPrecatED foo "use labels from generic-lens instead" #-}
