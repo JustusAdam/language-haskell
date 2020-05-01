914df9e65159cbd81f32c5133e35026c3072f443
sheaf <sam.derbyshire@gmail.com>
2020-05-01 02:03:17 +0200

    fix highlighting of ticks and qualified names

---
+++ test/tickets/T0071b.hs
@@ -0,0 +1,15 @@
+-- SYNTAX TEST "source.haskell" "Ticks in qualified names"
+
+
+
+f = A''.B'C.g `DE'.G'H.h` k XY'.A'B.+ z
+--  ^^^^^^^^   ^^^^^^^^     ^^^^^^^^ entity.name.namespace.haskell
+
+g = 'A.c + 'B.+ d
+--  ^      ^ - entity.name.namespace.haskell
+
+type S = A'.B.C'D.E `DE'.G'H.T` L XY'.A'B.+ Z
+--       ^^^^^^^^^   ^^^^^^^^     ^^^^^^^^ entity.name.namespace.haskell
+
+type T = 'A.C + 'B.+ D
+--       ^      ^ - entity.name.namespace.haskell
