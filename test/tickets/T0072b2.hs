07c48836c4b4f85a4dab6571335dcd5f9a611fb6
sheaf <sam.derbyshire@gmail.com>
2020-05-04 07:06:40 +0200

    improvements to foreign import/export declarations

--- test/tickets/T0072b2.hs
+++ test/tickets/T0072b2.hs
@@ -2,15 +2,15 @@
 
 data B = B deriving A via B
 --         ^^^^^^^^ keyword.other.deriving.haskell
---                    ^^^ keyword.other.via.haskell
+--                    ^^^ keyword.other.deriving.strategy.via.haskell
 --                  ^     ^ storage.type.haskell
 data B = B deriving stock    ( Eq, Generic )
 --         ^^^^^^^^ keyword.other.deriving.haskell
---                  ^^^^^ keyword.other.deriving-strategy.haskell
+--                  ^^^^^ keyword.other.deriving.strategy.stock.haskell
 --                             ^^  ^^^^^^^ storage.type.haskell
 data B = B deriving anyclass NFData
 --         ^^^^^^^^ keyword.other.deriving.haskell
---                  ^^^^^^^^ keyword.other.deriving-strategy.haskell
+--                  ^^^^^^^^ keyword.other.deriving.strategy.anyclass.haskell
 --                           ^^^^^^ storage.type.haskell
 
 newtype StateMonad b c r = StateMonad (StateT (MyState (Something b c) b) IO r)
