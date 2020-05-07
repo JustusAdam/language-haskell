dcc997f78c22697e1a0e87a694d81361b803dbc4
sheaf <sam.derbyshire@gmail.com>
2020-05-07 21:42:01 +0200

    Fix type-lits type application

--- test/tests/UnusualTypeApplications.hs
+++ test/tests/UnusualTypeApplications.hs
@@ -23,6 +23,14 @@ f = g @'[A B, C]
 --    ^ keyword.operator.prefix.at.haskell
 --    ^^^^^^^^^^ meta.type-application.haskell
 
+f = g @123
+--    ^ keyword.operator.prefix.at.haskell
+--    ^^^^^^^^^^ meta.type-application.haskell
+
+f = g @"abc c\" de"
+--    ^ keyword.operator.prefix.at.haskell
+--    ^^^^^^^^^^^^^ meta.type-application.haskell
+
 f =   g@A
 --     ^^ - meta.type-application.haskell
 f =   +@A
