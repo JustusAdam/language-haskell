6ed6072a3dc66903e451ee80286c7fa162241975
sheaf <sam.derbyshire@gmail.com>
2020-05-08 21:45:22 +0200

    Require proper boundary for prefix operators

--- test/tests/WildcardExport.hs
+++ test/tests/WildcardExport.hs
@@ -1,7 +1,9 @@
 -- SYNTAX TEST "source.haskell" "Bug with wildcard export consuming a single parenthesis"
 
 module M
-  ( T
+  ( S(..)
+--    ^^ variable.other.member.wildcard.haskell
+  , T
     ( .. , ( ..! ), ( :.. ), P, Q )
 --    ^^ variable.other.member.wildcard.haskell
 --           ^^        ^^ - variable.other.member.wildcard.haskell
