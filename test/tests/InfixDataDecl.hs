4a7376fe142ba5de49d0423ed7996490d8ac1985
sheaf <sam.derbyshire@gmail.com>
2020-06-22 20:28:07 +0200

    Add extra test case for infix datacon highlighting

--- test/tests/InfixDataDecl.hs
+++ test/tests/InfixDataDecl.hs
@@ -13,6 +13,15 @@
 --                                        ^                      ^ punctuation.bracket.haskell
 
 
+    data A a b = '[ X, y, Z ] `C` D e
+--  ^^^^ keyword.other.data.haskell
+--             ^ keyword.operator.eq.haskell
+--               ^ keyword.operator.promotion.haskell
+--                             ^ constant.other.haskell
+--                            ^ ^ punctuation.backtick.haskell
+--                  ^     ^       ^ storage.type.haskell
+--         ^ ^         ^            ^ variable.other.generic-type.haskell
+
     data A a b = ABC.D `C` GH
 --  ^^^^ keyword.other.data.haskell
 --             ^ keyword.operator.eq.haskell
