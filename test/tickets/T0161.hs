05bfeff822d9079b3ddd28cc259d169405f07457
sheaf <sam.derbyshire@gmail.com>
2020-06-22 20:20:12 +0200

    Fix typo in data constructor highlighting regex
      * Fixes some problems with slow/frozen highlighting (see #161)

---
+++ test/tickets/T0161.hs
@@ -0,0 +1,25 @@
+-- SYNTAX TEST "source.haskell" "Data constructor highlighting"
+
+    data Foo a = (:>) a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a  
+--  ^^^^ keyword.other.data.haskell 
+--             ^ keyword.operator.eq.haskell
+--                ^^ constant.other.operator.haskell
+--           ^        ^ ^ ^                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             ^ ^ ^ variable.other.generic-type.haskell
+
+    data Foo a = (:>) `C` ( S, O ) deriving ( Show, Ord )
+--  ^^^^ keyword.other.data.haskell 
+--           ^ variable.other.generic-type.haskell
+--             ^ keyword.operator.eq.haskell
+--                ^^ storage.type.operator.haskell
+--                ^^ - constant.other.operator.haskell
+--                     ^ constant.other.haskell
+--                                 ^^^^^^^^ keyword.other.deriving.haskell
+--       ^^^                ^  ^              ^^^^  ^^^ storage.type.haskell
+
+    data Foo a = (:>) Bar a deriving ( Show, Ord )
+--  ^^^^ keyword.other.data.haskell 
+--             ^ keyword.operator.eq.haskell
+--                ^^ constant.other.operator.haskell
+--           ^            ^ variable.other.generic-type.haskell
+--                          ^^^^^^^^ keyword.other.deriving.haskell
+--       ^^^          ^^^              ^^^^  ^^^ storage.type.haskell
