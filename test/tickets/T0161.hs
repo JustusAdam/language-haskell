53fce3754a51359abf4ccfbd42823203cc63d349
sheaf <sam.derbyshire@gmail.com>
2026-06-05 11:57:14 +0200

    Add tricky test case to T0161 test

--- test/tickets/T0161.hs
+++ test/tickets/T0161.hs
@@ -1,13 +1,13 @@
 -- SYNTAX TEST "source.haskell" "Data constructor highlighting"
 
     data Foo a = (:>) a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a  
---  ^^^^ keyword.other.data.haskell 
+--  ^^^^ keyword.other.data.haskell
 --             ^ keyword.operator.eq.haskell
 --                ^^ constant.other.operator.prefix.haskell
 --           ^        ^ ^ ^                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             ^ ^ ^ variable.other.generic-type.haskell
 
     data Foo a = (:>) `C` ( S, O ) deriving ( Show, Ord )
---  ^^^^ keyword.other.data.haskell 
+--  ^^^^ keyword.other.data.haskell
 --           ^ variable.other.generic-type.haskell
 --             ^ keyword.operator.eq.haskell
 --                ^^ storage.type.operator.haskell
@@ -17,9 +17,22 @@
 --       ^^^                ^  ^              ^^^^  ^^^ storage.type.haskell
 
     data Foo a = (:>) Bar a deriving ( Show, Ord )
---  ^^^^ keyword.other.data.haskell 
+--  ^^^^ keyword.other.data.haskell
 --             ^ keyword.operator.eq.haskell
 --                ^^ constant.other.operator.prefix.haskell
 --           ^            ^ variable.other.generic-type.haskell
 --                          ^^^^^^^^ keyword.other.deriving.haskell
 --       ^^^          ^^^              ^^^^  ^^^ storage.type.haskell
+
+
+    data instance Box @( SumRep '[ r1, r2 ] ) a
+      = BoxSum2_0 ( Box ( a `IndexSum` 0 ) )
+--      ^^^^^^^^^ constant.other.haskell
+--                  ^^^ storage.type.haskell
+--                        ^ variable.other.generic-type.haskell
+--                           ^^^^^^^^ storage.type.infix.haskell
+      | BoxSum2_1 ( Box ( a `IndexSum` 1 ) )
+--      ^^^^^^^^^ constant.other.haskell
+--                  ^^^ storage.type.haskell
+--                        ^ variable.other.generic-type.haskell
+--                           ^^^^^^^^ storage.type.infix.haskell
